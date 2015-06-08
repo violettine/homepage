5472fd0d0772bf2cbd52e9c24db913c08d4da34a
violettine <violettine@freenet.de>
2015-06-08 18:32:11 +0200

    edit gemfile for heroku

--- app/controllers/personal_page_controller.rb
+++ app/controllers/personal_page_controller.rb
@@ -1,5 +1,5 @@
 class PersonalPageController < ApplicationController
 	def index
-		@bubbling_variable = 'I am '+ ['blue','red', 'yellow', 'green', 'purple'].sample + '!'
+		@bubbling_variable = 'I am '+ ['blue','red', 'yellow', 'green', 'purple', 'orange'].sample + '!'
 	end
 end
