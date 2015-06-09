7d3bc0f0f493606862e9cefd322bebaa0fddcd4f
violettine <violettine@freenet.de>
2015-06-09 18:03:00 +0200

    tested a lot more with bubble_thingi, but nothing worked

--- app/controllers/build_page_controller.rb
+++ app/controllers/build_page_controller.rb
@@ -1,4 +1,4 @@
 class BuildPageController < ApplicationController
-	def index
-	end
+  def index
+  end
 end
--- app/controllers/personal_page_controller.rb
+++ app/controllers/personal_page_controller.rb
@@ -1,7 +1,6 @@
 class PersonalPageController < ApplicationController
-	def index
-		#@bubbling_variable = 'I am '+ ['blue','red', 'yellow', 'green', 'purple', 'orange'].sample + '!'
-		@bubbling_variable = 'I am '+ %w[blue,red, yellow, green, purple, orange].sample + '!'
-
-	end
+  def index
+    # @bubbling_variable = 'I am '+ ['blue','red', 'yellow', 'green', 'purple', 'orange'].sample + '!'
+    @bubbling_variable = 'I am ' + %w[blue red yellow green purple orange].sample + '!'
+  end
 end
