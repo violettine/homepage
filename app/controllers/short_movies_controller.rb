1de8736952678699d7256085c5b487a5784fb8b2
violettine <violettine@freenet.de>
2015-06-11 18:15:40 +0200

    added model to handle my short_movies

--- app/controllers/short_movies_controller.rb
+++ app/controllers/short_movies_controller.rb
@@ -1,4 +1,20 @@
 class ShortMoviesController < ApplicationController
   def show
   end
+
+  def create
+    @short_movie = ShortMovies.new(short_movies_require)
+
+    if @short_movie.save
+      puts 'Video saved!'
+    else
+      puts 'Video not saved!'
+    end
+  end
+
+  private
+
+  def short_movies_require
+    params.require(:short_movies).permit(:short_movie)
+  end
 end
---
+++ test/models/short_movie_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ShortMovieTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
