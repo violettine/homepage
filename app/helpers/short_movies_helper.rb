874848529e552601034877698872bcd18091b9c8
violettine <violettine@freenet.de>
2015-06-15 19:11:36 +0200

    add link to build/index and refactor conditional expression to guard clause

--- app/helpers/short_movies_helper.rb
+++ app/helpers/short_movies_helper.rb
@@ -1,8 +1,6 @@
 module ShortMoviesHelper
   def sub_link_address(movie)
-    unless movie.link.match(/embed/)
-      # replace watch?v= with embed/ to embed the video
-      movie.link.sub!('watch?v=', 'embed/')
-    end
+    # replace watch?v= with embed/ to embed the video
+    movie.link.sub!('watch?v=', 'embed/') unless movie.link.match(/embed/)
   end
 end
