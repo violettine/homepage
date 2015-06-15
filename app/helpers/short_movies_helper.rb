dd0796ae140f1748af6d1b241ffe6aea4aef587b
violettine <violettine@freenet.de>
2015-06-15 12:40:00 +0200

    refactor short_movie show in helper

--- app/helpers/short_movies_helper.rb
+++ app/helpers/short_movies_helper.rb
@@ -1,2 +1,8 @@
 module ShortMoviesHelper
+  def sub_link_address(movie)
+    unless movie.link.match(/embed/)
+      # replace watch?v= with embed/ to embed the video
+      movie.link.sub!('watch?v=', 'embed/')
+    end
+  end
 end
