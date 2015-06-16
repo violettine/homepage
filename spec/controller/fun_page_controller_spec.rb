a337098cb4a5003625fd348981287a4d4e060d99
violettine <violettine@freenet.de>
2015-06-16 11:23:03 +0200

    create fun_page_ and personal_page_spec, changed rspec helper files to match rubocop style better

---
+++ spec/controller/fun_page_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe(FunPageController, type: :controller) do
+  describe 'GET #index' do
+    it 'responds with an HTTP 200 status code' do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+
+    it 'creates successfully @bubbling_variable' do
+      @test_bubbling_variable =
+        ('I am ' + %(w[blue red yellow green purple orange].sample) + '!')
+      get :index
+
+      expect(@test_bubbling_variable).to match(
+        /^[I am]{4}.(blue|red|yellow|green|purple|orange)[!]$/)
+    end
+  end
+end
