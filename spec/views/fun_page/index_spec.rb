41d0bb83a3f503e675e237afe35572baac2fcf44
violettine <violettine@freenet.de>
2015-06-17 12:38:47 +0200

    some style big fixes

--- spec/views/fun_page/index_spec.rb
+++ spec/views/fun_page/index_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe('fun_page/index', type: :view) do 
-  it 'has a header' do 
+RSpec.describe('fun_page/index', type: :view) do
+  it 'has a header' do
     render
     assert_select 'h1'
   end
 
-  it 'renders short_movies template' do 
+  it 'renders short_movies template' do
     render
-    expect(rendered).to match /Movies/
+    expect(rendered).to match(/Movies/)
   end
 end
--- spec/views/personal_page/index_spec.rb
+++ spec/views/personal_page/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe('personal_page/index', type: :view) do 
+RSpec.describe('personal_page/index', type: :view) do
   it 'displays a h1 header' do
     render
     assert_select 'h1'
@@ -8,6 +8,6 @@ RSpec.describe('personal_page/index', type: :view) do
 
   it 'displays one link' do
     render
-    assert_select '<a href', count:1
+    assert_select 'a', count: 1
   end
 end
