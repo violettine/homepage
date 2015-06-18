19f95410115d3e132c4f9bbadb3cddc4006638b6
violettine <violettine@freenet.de>
2015-06-18 10:27:21 +0200

    weather pai works with diff cities now

--- app/helpers/api_page_helper.rb
+++ app/helpers/api_page_helper.rb
@@ -1,11 +1,15 @@
 module ApiPageHelper
   def getJson(city='berlin')
-    data = open('http://api.openweathermap.org/data/2.5/weather?q=berlin')
+    data = open('http://api.openweathermap.org/data/2.5/weather?q='+city)
     json_data=JSON.parse(data.read)
     if json_data['message']
       puts 'time for a problem!'
+      puts''
+      puts''
+      puts''
+      puts''
     else
-      @weather = json_data['main']['temp']
+      @weather = json_data
     end
   end
 end
