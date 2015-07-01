7c96229d51f2fcbaddca16729a780deb875c9c24
violettine <violettine@freenet.de>
2015-07-01 10:28:37 +0200

    changed https in jquery bib und highcharts bib to https, cause compile in production otherwise blocked, insecure connection

--- app/assets/javascripts/call_draw_daily_chart.js
+++ app/assets/javascripts/call_draw_daily_chart.js
@@ -1,6 +1,4 @@
-
-
-$(document).ready(function(){ //the same as $(document).ready
+$(function(){ //the same as $(document).ready
   var d = $('.temp_information').data('temp');
   daily = d.list;
   showDailyChart(daily);
