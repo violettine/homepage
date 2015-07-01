6c9f27f822b3a0ec8b41845e116e23da39175975
violettine <violettine@freenet.de>
2015-07-01 14:05:47 +0200

    fixed bug

--- spec/views/pages/fun_spec.rb
+++ spec/views/pages/fun_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe('pages/fun', type: :view) do
   it 'has a h1 header' do
+    render
     assert_select 'h1'
   end
 
   it 'renders short_movies template' do
+    render
     expect(rendered).to match(/short movies/)
   end
 end
